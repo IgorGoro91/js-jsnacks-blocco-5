10b69c17958d0946c21817f66ee5a0ef0489fe3e
IgorGoro91 <igorysc@gmail.com>
2025-01-27 18:08:47 +0100

    add:snack1

---
+++ snack-1/script.js
@@ -0,0 +1,8 @@
+const names = ['Edoardo', 'Simone', 'Francesco'];
+
+names.forEach( (element ) => {
+    console.log(element)
+})
+
+// Stampa in console tutti i nomi
+// Risultato: 'Edoardo', 'Simone', 'Francesco'
